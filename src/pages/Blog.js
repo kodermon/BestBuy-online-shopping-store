06ad4d2a1884a0ff7b3df02cc1ff77003f45c2e5
9ghtCoder <lordkenest@gmail.com>
2022-12-17 11:40:27 -0800

    little change in the about

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -6,7 +6,7 @@ const Blog = () => {
     <Stack width='80%' alignItems='center' m='auto' textAlign='justify' p='70px 0'>
         <Typography fontSize='32px' fontWeight='600' mb='20px'>About BestBuy</Typography>
         <Typography fontSize='23px'>
-          BestBuy is an imaginary e-commerce web app built with React and Material UI, using the <a href='https://fakestoreapi.com/' target='_blank' className='fake-api-link'>Fake Store API</a>. 
+          BestBuy is an imaginary e-commerce web app built with React, Material UI and data from <a href='https://fakestoreapi.com/' target='_blank' className='fake-api-link'>Fake Store API</a>. 
         </Typography>
     </Stack>
   )
